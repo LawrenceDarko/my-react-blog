ab1e3a45d961d7ed78e5b46142323b3b9a27a6e6
LawrenceDarko <darkolawrence@gmail.com>
2021-11-01 10:44:30 +0800

    app structural design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar.js';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Components/Create';
+import BlogDetails from './Components/BlogDetails';
+import Page404 from './Components/Page404';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+      <Navbar/>
+      <div className="content">
+        <Switch>
+          <Route exact path="/" >
+            <Home/>
+          </Route>
+          <Route path="/create" >
+            <Create />
+          </Route>
+          <Route path="/blogs/:id" >
+            <BlogDetails />
+          </Route>
+          <Route path="*" >
+            <Page404 />
+          </Route>
+        </Switch>
+      </div>
+
     </div>
+    </Router>
   );
 }
 
